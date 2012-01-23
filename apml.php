df53a0ef9769705bac67a7e9bcf6c7f04f0b0ebf
pfefferle <pfefferle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-23 10:49:04 +0000

    better host-meta and webfinger support
    git-svn-id: https://plugins.svn.wordpress.org/apml/trunk@493902 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- apml.php
+++ apml.php
@@ -3,7 +3,7 @@
 Plugin Name: APML
 Plugin URI: http://notizblog.org/projects/apml-for-wordpress/
 Description: This plugin creates an APML Feed using the WordPress tags, categories, links and feeds.
-Version: 3.1.3
+Version: 3.1.4
 Author: Matthias Pfefferle
 Author URI: http://notizblog.org/
 */
